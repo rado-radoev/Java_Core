c957ffd3d883c896ba61010cede97e41d21ec3c5
Radoslav Radoev <rradoev@gmail.com>
2017-10-02 23:22:17 -0700

    Picture and pixel test

---
+++ Eclipse-Dev/_Core_Assignment1/src/Picture.java
@@ -0,0 +1,145 @@
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.text.*;
+import java.util.*;
+import java.util.List; // resolves problem with java.awt.List and java.util.List
+
+/**
+ * A class that represents a picture.  This class inherits from 
+ * SimplePicture and allows the student to add functionality to
+ * the Picture class.  
+ * 
+ * Copyright Georgia Institute of Technology 2004-2005
+ * @author Barbara Ericson ericson@cc.gatech.edu
+ */
+public class Picture extends SimplePicture 
+{
+  ///////////////////// constructors //////////////////////////////////
+  
+  /**
+   * Constructor that takes no arguments 
+   */
+  public Picture ()
+  {
+    /* not needed but use it to show students the implicit call to super()
+     * child constructors always call a parent constructor 
+     */
+    super();  
+  }
+  
+  /**
+   * Constructor that takes a file name and creates the picture 
+   * @param fileName the name of the file to create the picture from
+   */
+  public Picture(String fileName)
+  {
+    // let the parent class handle this fileName
+    super(fileName);
+  }
+  
+  /**
+   * Constructor that takes the width and height
+   * @param width the width of the desired picture
+   * @param height the height of the desired picture
+   */
+  public Picture(int width, int height)
+  {
+    // let the parent class handle this width and height
+    super(width,height);
+  }
+  
+  /**
+   * Constructor that takes a picture and creates a 
+   * copy of that picture
+   */
+  public Picture(Picture copyPicture)
+  {
+    // let the parent class do the copy
+    super(copyPicture);
+  }
+  
+  /**
+   * Constructor that takes a buffered image
+   * @param image the buffered image to use
+   */
+  public Picture(BufferedImage image)
+  {
+    super(image);
+  }
+  
+  ////////////////////// methods ///////////////////////////////////////
+  
+  /**
+   * Method to return a string with information about this picture.
+   * @return a string with information about the picture such as fileName,
+   * height and width.
+   */
+  public String toString()
+  {
+    String output = "Picture, filename " + getFileName() + 
+      " height " + getHeight() 
+      + " width " + getWidth();
+    return output;
+    
+  }
+  
+  
+  public static void main(String[] args) 
+  {
+     String fileName = FileChooser.pickAFile();
+     Picture pictObj = new Picture(fileName);
+     pictObj.explore();
+  }
+  
+  /**
+   * Method to decrease the red by half in the current picture
+   */
+  public void decreaseRedHalf() {
+	  Pixel[] pixelArray = this.getPixels();
+	  Pixel pixel = null;
+	  int value = 0;
+	  int index = 0;
+	  
+	  // loop through all the pixels
+	  while (index < pixelArray.length) {
+		  // get the current pixel
+		  pixel = pixelArray[index];
+		  
+		  // get the value
+		  value = pixel.getRed();
+		  
+		  // decrease the red value by 50% (1/2)
+		  value = (int) (value * 0.5);
+		  
+		  // set the red value of the current pixel to the new value
+		  pixel.setRed(value);
+		  
+		  // increment the index
+		  index = index + 1;
+	  }
+  }
+  
+	public void decreaseRed() {
+		Pixel[] pixelArray = this.getPixels();
+		
+		int value = 0;
+		
+		// loop through all the pixel in the array
+		for (Pixel pixel : pixelArray) {
+			
+			// get the red value
+			value = pixel.getRed();
+			
+			// decrease the red value by 50% (1/2)
+			value = (int) (value * 0.5);
+			
+			// set the red value of the current pixel to the new value
+			pixel.setRed(value);
+		}
+	}
+  
+  
+} // this } is the end of class Picture, put all new methods before this
+ 
---
+++ Eclipse-Dev/_Core_Assignment1/src/PixelTester.java
@@ -0,0 +1,89 @@
+import java.awt.Color;
+
+public class PixelTester {
+
+	public static void main(String[] args) {
+		String fileName = "/Users/superlamer/GitHub/Java_Core/Media Computation book source/mediasources-no-movies-7-30-06/intro-prog-java/mediasources/caterpillar.jpg";
+		Picture picture = new Picture(fileName);
+		picture.decreaseRedHalf();
+		picture.show();
+	}
+	
+	public static void testMethod5() {
+		String fileName = "/Users/superlamer/GitHub/Java_Core/Media Computation book source/mediasources-no-movies-7-30-06/intro-prog-java/mediasources/caterpillar.jpg";
+		Picture picture = new Picture(fileName);
+		picture.show();
+		Pixel[] pixelArray = picture.getPixels();
+		Pixel pixel = null;
+		int index = 0;
+		while (index < pixelArray.length) {
+			pixel = pixelArray[index];
+			pixel.setColor(Color.BLACK);
+			index+=10;
+		}
+		picture.repaint();
+	}
+	
+	public static void testMethod4() {
+		String fileName = "/Users/superlamer/GitHub/Java_Core/Media Computation book source/mediasources-no-movies-7-30-06/intro-prog-java/mediasources/caterpillar.jpg";
+		Picture picture = new Picture(fileName);
+		picture.show();
+		picture.getPixel(10, 100).setColor(Color.black);
+		picture.getPixel(11, 100).setColor(Color.black);
+		picture.getPixel(12, 100).setColor(Color.black);
+		picture.getPixel(13, 100).setColor(Color.black);
+		picture.getPixel(14, 100).setColor(Color.black);
+		picture.getPixel(15, 100).setColor(Color.black);
+		picture.getPixel(16, 100).setColor(Color.black);
+		picture.getPixel(17, 100).setColor(Color.black);
+		picture.getPixel(18, 100).setColor(Color.black);
+		picture.getPixel(19, 100).setColor(Color.black);
+		picture.repaint();
+		picture.explore();
+	}
+	
+	public static void testMethod3() {
+		Color pickedColorObj = ColorChooser.pickAColor();
+		System.out.println(pickedColorObj);
+	}
+	
+	public static void testMethod2() {
+		Color testColorObject = new Color(168,131,105);
+		System.out.println(testColorObject);
+	
+		testColorObject = testColorObject.darker();
+		System.out.println(testColorObject);
+
+		testColorObject = testColorObject.brighter();
+		System.out.println(testColorObject);
+	
+	}
+	
+	public static void testMethod() {
+		String fileName = "/Users/superlamer/GitHub/Java_Core/Media Computation book source/mediasources-no-movies-7-30-06/intro-prog-java/mediasources/caterpillar.jpg";
+		Picture pictureObject = new Picture(fileName);
+		
+		Pixel pixelObject =  pictureObject.getPixel(0, 0);
+		
+		System.out.println(pixelObject);
+		
+		Pixel[] pixelArray = pictureObject.getPixels();
+		System.out.println(pixelArray[1]);
+		
+		System.out.println("x: " + pixelObject.getX() + " y: " + pixelObject.getY());
+		
+		System.out.println(pixelObject.getRed());
+		pixelObject.setRed(0);
+		System.out.println(pixelObject.getRed());
+		
+		Color colorObject = pixelObject.getColor();
+		System.out.println(colorObject);
+		pixelObject.setColor(new Color(0,100,0));
+		System.out.println(pixelObject.getColor());
+	
+	
+		System.out.println(pictureObject.getPixel(0, 0));
+		
+		pictureObject.write(fileName + ".jpeg");
+	}
+}
