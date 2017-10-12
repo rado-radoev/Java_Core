0562c5b3534e8e46077b41803e10e3a3e7eedfdd
Radoslav Radoev <rradoev@gmail.com>
2017-10-12 09:49:28 -0700

    Assignment 3 -wip

--- Eclipse-Dev/_Core_Assignment3/src/PictureTest.java
+++ Eclipse-Dev/_Core_Assignment3/src/PictureTest.java
@@ -2,9 +2,9 @@
 public class PictureTest {
 
 	public static void main(String[] args) {
-		Picture pic = 
-				new Picture("barbara.jpg");
-		pic.blurr(83, 73, 100, 100, 2);
+		Picture pic = new Picture(
+				"C:\\GitHub\\Java_Core\\Media Computation book source\\mediasources-no-movies-7-30-06\\intro-prog-java\\mediasources\\barbara.jpg");
+		pic.blurr(83, 73, 100, 100, 10);
 		pic.explore();
 	}
 
