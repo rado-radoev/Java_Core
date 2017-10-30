b3f7d6878459bd0784541f13ce443232e8b02a62
Radoslav Radoev <rradoev@gmail.com>
2017-10-30 13:37:10 -0700

    removed unused variables and methods from read poll

--- Eclipse-Dev/Core_ex7_StudentPoll/src/ReadPoll.java
+++ Eclipse-Dev/Core_ex7_StudentPoll/src/ReadPoll.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthStyle;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream.GetField;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +8,6 @@ import java.util.Formatter;
 
 public class ReadPoll {
 
-	private Formatter output;
 	private String file = "numbers.txt";
 	
 	public String getFile() {
@@ -27,35 +22,24 @@ public class ReadPoll {
 	public static void main(String[] args) {
 		ReadPoll rp = new ReadPoll();
 		
-		//rp.openFile();
+		rp.checkFile();
 		rp.outputResults();
-		rp.closeFile();
 	}
 
 
-	public void openFile() {
+	public void checkFile() {
 		Path path = Paths.get(file);
 		
 		if (!Files.exists(path)) {
 			System.out.println("File does not exist. Try creating it first. Terminating ...");
 			System.exit(1);
 		}
-		
-		try {
-			output = new Formatter(file);
-		} catch (FileNotFoundException fnf) {
-			System.out.println("File does not exist. Try creating it first. Terminating ...");
-			System.exit(1);
-		} catch (SecurityException se) {
-			System.out.println("Cannot access the file. Terminating ...");
-			System.exit(1);
-		}
 	}
 	
 	public void outputResults() {
 		int[] answers = readFile();
-		for (int i = 0; i < answers.length; i++) {
-			System.out.println("Answer " + i + " is: " + answers[i]);
+		for (int i = 1; i < answers.length; i++) {
+			System.out.println("Answer " + i + " voted: " + answers[i] + " times");
 		}
 	}
 	
@@ -80,11 +64,5 @@ public class ReadPoll {
 		
 		return frequencey;
 	}
-	
-	
-	public void closeFile() {
-		if (output != null) {
-			output.close();
-		}
-	}
+
 }
---
+++ Eclipse-Dev/_Core_Tests/src/MouseClick.java
@@ -0,0 +1,8 @@
+
+public class MouseClick {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
